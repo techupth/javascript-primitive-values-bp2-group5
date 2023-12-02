15109b2ff3c5f59d6e40225791a7a59e03cc5771
Dhammarin <ueang@Dhammarins-MacBook-Air.local>
2023-12-02 22:45:15 +0700

    feat(exercise): add completed exercise 1-8 (primitive valuues)

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalProductPrice=27000;
+let discount= 0.25*totalProductPrice;
+let NetProductPrice= totalProductPrice-discount;
+console.log(NetProductPrice);
