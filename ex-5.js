e38d04e3c6520704adef81045ff20adbeee683e3
Natthapon P <natthaxp@hotmail.com>
2024-01-06 17:13:50 +0700

    Feat: Add completed exercise 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let checkoutPrice = 27000;
+let discountPrice = checkoutPrice * 25 / 100;
+let totalPrice = checkoutPrice - discountPrice;
+
+//console.log(totalPrice);
